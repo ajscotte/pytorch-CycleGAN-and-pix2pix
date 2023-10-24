e05a1db7961a39f714e84211db6b70bb2b32e273
ajs667 <ajscotte@gmail.com>
2023-10-24 01:31:18 -0400

    changed hook

--- models/fed_pix2pix_model.py
+++ models/fed_pix2pix_model.py
@@ -57,10 +57,10 @@ class FedPix2PixModel(BaseModel):
             self.model_names = ['G']
             
         #defining virtual workers
-        # hook = sy.TorchHook(torch) 
-        # self.worker1 = sy.VirtualWorker(hook, id="intel")
-        domain = sy.Domain(name="my_domain")
-        domain.add_worker(name="worker_1", address="127.0.0.1:10000")
+        hook = sy.TorchHook(torch) 
+        self.worker1 = sy.VirtualWorker(hook, id="intel")
+        # domain = sy.Domain(name="my_domain")
+        # domain.add_worker(name="worker_1", address="127.0.0.1:10000")
 
         
         # self.worker1  = sy.RemoteWorker("intel", "127.0.0.1", random.randint(10000, 20000))
