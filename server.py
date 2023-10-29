0ace6238308b8ea603ef5829b567a9eedb6999b4
ajs667 <ajscotte@gmail.com>
2023-10-29 15:20:30 -0400

    small changed

--- server.py
+++ server.py
@@ -3,5 +3,5 @@ import flwr as fl
 fl.server.start_server(
     server_address="0.0.0.0:8080",
     config=fl.server.ServerConfig(num_rounds=3),
-    strategy=fl.server.strategy.ParallelStrategy(), 
+    strategy=fl.server.strategy.FedAvg(), 
 )
