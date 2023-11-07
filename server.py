9d27c4bd13086ee2297c68e93c71ce49279931cd
ajs667 <ajscotte@gmail.com>
2023-11-06 20:30:32 -0500

    flower

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import flwr as fl
 
-# fl.server.start_server(
-#     server_address="0.0.0.0:8080",
-#     config=fl.server.ServerConfig(num_rounds=3),
-#     strategy=fl.server.strategy.FedAvg(), 
-# )
+fl.server.start_server(
+    server_address="0.0.0.0:8080",
+    config=fl.server.ServerConfig(num_rounds=3),
+    strategy=fl.server.strategy.FedAvg(min_available_clients=1), 
+)
 
-fl.server.start_server("localhost:8080", config={"num_rounds": 3})
+# fl.server.start_server("localhost:8080", config={"num_rounds": 3})
