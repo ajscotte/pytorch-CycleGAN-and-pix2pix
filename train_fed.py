55d1a10d67c2040628d89e24ae0b198e3713c509
ajs667 <ajscotte@gmail.com>
2023-11-06 21:02:32 -0500

    got rid of uodates for test

--- train_fed.py
+++ train_fed.py
@@ -72,27 +72,27 @@ def train(model, dataset, opt):
 
             #plan add logic to stop if validation set starts to increase in loss meaning overfitting has occured
             
-            if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
-                save_result = total_iters % opt.update_html_freq == 0
-                model.compute_visuals()
-                visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
+        #     if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
+        #         save_result = total_iters % opt.update_html_freq == 0
+        #         model.compute_visuals()
+        #         visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
-            if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
-                losses = model.get_current_losses()
-                t_comp = (time.time() - iter_start_time) / opt.batch_size
-                visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
-                if opt.display_id > 0:
-                    visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
+        #     if total_iters % opt.print_freq == 0:    # print training losses and save logging information to the disk
+        #         losses = model.get_current_losses()
+        #         t_comp = (time.time() - iter_start_time) / opt.batch_size
+        #         visualizer.print_current_losses(epoch, epoch_iter, losses, t_comp, t_data)
+        #         if opt.display_id > 0:
+        #             visualizer.plot_current_losses(epoch, float(epoch_iter) / dataset_size, losses)
 
-            if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
-                print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
-                save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
-                model.save_networks(save_suffix)
+        #     if total_iters % opt.save_latest_freq == 0:   # cache our latest model every <save_latest_freq> iterations
+        #         print('saving the latest model (epoch %d, total_iters %d)' % (epoch, total_iters))
+        #         save_suffix = 'iter_%d' % total_iters if opt.save_by_iter else 'latest'
+        #         model.save_networks(save_suffix)
 
-            iter_data_time = time.time()
-        if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
-            print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
-            model.save_networks('latest')
-            model.save_networks(epoch)
+        #     iter_data_time = time.time()
+        # if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
+        #     print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
+        #     model.save_networks('latest')
+        #     model.save_networks(epoch)
 
         print('End of epoch %d / %d \t Time Taken: %d sec' % (epoch, opt.n_epochs + opt.n_epochs_decay, time.time() - epoch_start_time))
