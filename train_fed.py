f2ccc8f4635abb3e6b975a20919582a6cbc97e9e
ajs667 <ajscotte@gmail.com>
2023-11-06 21:33:15 -0500

    opt switch

--- train_fed.py
+++ train_fed.py
@@ -69,23 +69,23 @@ def train(model, dataset, opt):
         print("update learn")
         model.update_learning_rate()    # update learning rates in the beginning of every epoch.
         print("dataset loop")
-        print(dataset)
-        for i, data in enumerate(dataset):  # inner loop within one epoch
-            print("start loop")
-            iter_start_time = time.time()  # timer for computation per iteration
-            print("freq")
-            if total_iters % opt.print_freq == 0:
-                t_data = iter_start_time - iter_data_time
+        # print(dataset)
+        # for i, data in enumerate(dataset):  # inner loop within one epoch
+        #     print("start loop")
+        #     iter_start_time = time.time()  # timer for computation per iteration
+        #     print("freq")
+        #     if total_iters % opt.print_freq == 0:
+        #         t_data = iter_start_time - iter_data_time
 
-            total_iters += opt.batch_size
-            epoch_iter += opt.batch_size
+        #     total_iters += opt.batch_size
+        #     epoch_iter += opt.batch_size
             
-            print("model updates")
-            # model.set_input(data)         # unpack data from dataset and apply preprocessing
-            # model.optimize_parameters()   # calculate loss functions, get gradients, update network weights !!also updates!!
+        #     print("model updates")
+        #     # model.set_input(data)         # unpack data from dataset and apply preprocessing
+        #     # model.optimize_parameters()   # calculate loss functions, get gradients, update network weights !!also updates!!
 
-            #plan add logic to stop if validation set starts to increase in loss meaning overfitting has occured
-            print("done here")
+        #     #plan add logic to stop if validation set starts to increase in loss meaning overfitting has occured
+        #     print("done here")
         #     if total_iters % opt.display_freq == 0:   # display images on visdom and save images to a HTML file
         #         save_result = total_iters % opt.update_html_freq == 0
         #         model.compute_visuals()
