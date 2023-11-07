ebf853afe9f591c2d39786c5f819f73877e29798
ajs667 <ajscotte@gmail.com>
2023-11-07 02:43:36 -0500

    why

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import flwr as fl
 fl.server.start_server(
     server_address="0.0.0.0:8080",
     config=fl.server.ServerConfig(num_rounds=3),
-    strategy=fl.server.strategy.FedAvg(), 
+    strategy=fl.server.strategy.FedAvg(min_available_clients=1, min_evaluate_clients=1, min_fit_clients=1), 
 )
 
 # fl.server.start_server("localhost:8080", config={"num_rounds": 3})
