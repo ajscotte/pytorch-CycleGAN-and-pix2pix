af1e5da3251ffe7f191d111dcd74f80a88d0ccb1
ajs667 <ajscotte@gmail.com>
2023-12-09 18:54:14 -0500

    find

--- find_file.py
+++ find_file.py
@@ -1,3 +1,47 @@
+# import os
+# import fnmatch
+# import cv2
+# import numpy as np
+# import matplotlib.pyplot as plt
+
+# def find_files(directory, pattern):
+#     matching_files = []
+#     for root, dirs, files in os.walk(directory):
+#         for filename in fnmatch.filter(files, pattern):
+#             matching_files.append(os.path.join(root, filename))
+#     return matching_files
+
+# def compare_images(image1_path, image2_path):
+#     # Read images
+#     img1 = cv2.imread(image1_path)
+#     img2 = cv2.imread(image2_path)
+
+#     # Check if the images have the same dimensions
+#     if img1.shape != img2.shape:
+#         print("Images have different dimensions. Cannot compare.")
+#         return
+
+#     # Calculate Mean Squared Error (MSE)
+#     mse = np.sum((img1 - img2) ** 2) / float(img1.size)
+
+#     return mse
+
+# def main():
+#     directory = '/home/ajs667/federated_pix2pix_real/pytorch-CycleGAN-and-pix2pix/results/central_nocrop/test_latest/images'
+#     pattern = '*fake*'
+    
+#     matching_files = find_files(directory, pattern)
+
+#     if matching_files:
+#         print("Matching files:")
+#         for file in matching_files:
+#             print(file)
+#     else:
+#         print("No matching files found.")
+
+# if __name__ == "__main__":
+#     main()
+    
 import os
 import fnmatch
 
@@ -5,11 +49,11 @@ def find_files(directory, pattern):
     matching_files = []
     for root, dirs, files in os.walk(directory):
         for filename in fnmatch.filter(files, pattern):
-            matching_files.append(os.path.join(root, filename))
+            matching_files.append(filename)
     return matching_files
 
 def main():
-    directory = '/home/ajs667/federated_pix2pix_real/pytorch-CycleGAN-and-pix2pix/results/central_nocrop/test_latest/images'
+    directory = '/path/to/directory'
     pattern = '*fake*'
     
     matching_files = find_files(directory, pattern)
@@ -23,4 +67,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
