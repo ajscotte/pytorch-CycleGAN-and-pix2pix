2c0a48753a2777cd3bc5ecd846526709edcd1084
ajs667 <ajscotte@gmail.com>
2023-12-09 19:00:46 -0500

    find

--- find_file.py
+++ find_file.py
@@ -1,49 +1,24 @@
-# import os
-# import fnmatch
-# import cv2
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# def find_files(directory, pattern):
-#     matching_files = []
-#     for root, dirs, files in os.walk(directory):
-#         for filename in fnmatch.filter(files, pattern):
-#             matching_files.append(os.path.join(root, filename))
-#     return matching_files
-
-# def compare_images(image1_path, image2_path):
-#     # Read images
-#     img1 = cv2.imread(image1_path)
-#     img2 = cv2.imread(image2_path)
-
-#     # Check if the images have the same dimensions
-#     if img1.shape != img2.shape:
-#         print("Images have different dimensions. Cannot compare.")
-#         return
-
-#     # Calculate Mean Squared Error (MSE)
-#     mse = np.sum((img1 - img2) ** 2) / float(img1.size)
-
-#     return mse
-
-# def main():
-#     directory = '/home/ajs667/federated_pix2pix_real/pytorch-CycleGAN-and-pix2pix/results/central_nocrop/test_latest/images'
-#     pattern = '*fake*'
-    
-#     matching_files = find_files(directory, pattern)
-
-#     if matching_files:
-#         print("Matching files:")
-#         for file in matching_files:
-#             print(file)
-#     else:
-#         print("No matching files found.")
-
-# if __name__ == "__main__":
-#     main()
-    
 import os
 import fnmatch
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def compare_images(image1_path, image2_path):
+    # Read images
+    img1 = cv2.imread(image1_path)
+    img2 = cv2.imread(image2_path)
+
+    # Check if the images have the same dimensions
+    if img1.shape != img2.shape:
+        print("Images have different dimensions. Cannot compare.")
+        return
+
+    # Calculate Mean Squared Error (MSE)
+    mse = np.sum((img1 - img2) ** 2) / float(img1.size)
+
+    return mse
+
 
 def find_files(directory, pattern):
     matching_files = []
@@ -54,6 +29,7 @@ def find_files(directory, pattern):
 
 def main():
     directory_central = '/home/ajs667/federated_pix2pix_real/pytorch-CycleGAN-and-pix2pix/results/central_nocrop/test_latest/images'
+    directory_fed = '/home/ajs667/federated_pix2pix_real/pytorch-CycleGAN-and-pix2pix/results/A2B_5rounds_nocrop/5/test_latest/images'
     pattern = '*fake*'
     
     matching_files = find_files(directory_central, pattern)
@@ -62,8 +38,9 @@ def main():
         print("Matching files:")
         for file in matching_files:
             print(file)
-            path_with_filename = os.path.join(directory_central, file)
-            print("Path with filename:", path_with_filename)
+            cent_image = os.path.join(directory_central, file)
+            fed_image = os.path.join(directory_fed, file)
+            print(compare_images(cent_image, fed_image))
     else:
         print("No matching files found.")
 
